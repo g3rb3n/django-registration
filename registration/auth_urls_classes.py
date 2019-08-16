62a245b3597fc4e744b73bb2f730729bcc26dac8
Gerben <gerbenvaneerten@gmail.com>
2019-08-16 03:26:37 +0200

    Added reverse_lazy

--- registration/auth_urls_classes.py
+++ registration/auth_urls_classes.py
@@ -7,6 +7,7 @@ import textwrap
 import warnings
 
 from django.conf.urls import url
+from django.urls import reverse_lazy
 from django.contrib.auth import views as auth_views
 
 
@@ -54,7 +55,7 @@ urlpatterns = [
     url(r'^password/reset/confirm/(?P<uidb64>[0-9A-Za-z_\-]+)/'
         r'(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
         auth_views.PasswordResetConfirmView.as_view(
-            success_url='auth_password_reset_complete'
+            success_url=reverse_lazy('auth_password_reset_complete')
         ),
         name='auth_password_reset_confirm'),
 ]
